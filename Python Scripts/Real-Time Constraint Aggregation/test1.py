640037a8a08f5f6674e13e4857c34219a3399a04
Stephen Xu <Stephen.Xu@calpine.com>
2023-06-30 08:52:39 -0500

    removed dummy ttesters

--- Python Scripts/Real-Time Constraint Aggregation/test1.py
+++
@@ -1,8 +0,0 @@
-import time
-
-print("Before waiting")
-
-time.sleep(10)  # Wait for 10 seconds
-
-print("After waiting")
-print("test 1 complete")
--- Python Scripts/Real-Time Constraint Aggregation/test2.py
+++
@@ -1,8 +0,0 @@
-import time
-
-print("Before waiting")
-
-time.sleep(5)  # Wait for 10 seconds
-
-print("After waiting")
-print("test 1 complete")
