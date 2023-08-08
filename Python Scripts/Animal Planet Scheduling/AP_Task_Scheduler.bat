eec8885900aad55d7f16ac0b3c68d88e516f01df
Stephen Xu <Stephen.Xu@calpine.com>
2023-08-08 13:45:50 -0500

    quick changes to the batch script on task scheduler

--- Python Scripts/Animal Planet Scheduling/AP_Task_Scheduler.bat
+++ Python Scripts/Animal Planet Scheduling/AP_Task_Scheduler.bat
@@ -1,3 +1,2 @@
 call C:\ProgramData\Anaconda3\Scripts\activate calpine
-pause
-C:/Users/sx17600/.conda/envs/calpine/python.exe "//pzpwcmfs01/CA/11_Transmission Analysis/ERCOT/101 - Misc/CRR Limit Aggregates/Python Scripts/AnimalPlanet.py"
+C:/Users/sx17600/.conda/envs/calpine/python.exe "//pzpwcmfs01/CA/11_Transmission Analysis/ERCOT/101 - Misc/CRR Limit Aggregates/Python Scripts/Animal Planet Scheduling/AnimalPlanet.py"
